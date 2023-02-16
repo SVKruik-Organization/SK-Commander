0131d5226bb06667cd82e7935fe29ab1ca3dc6fd
Stefan Kruik <sv.kruik@gmail.com>
2023-02-16 09:02:07 +0100

    Skeleton Improved

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            sandbox: false,
+            spellcheck: true,
+        },
+        webPreferences: {
+            preload: path.join(__dirname, '/preload.js'),
+        },
     });
 
-    win.loadFile('index.html');
+    win.loadFile('./frontend/index.html');
 };
+
+app.setUserTasks([
+    {
+        program: process.execPath,
+        arguments: '--new-window',
+        iconPath: process.execPath,
+        iconIndex: 0,
+        title: 'New Window',
+        description: 'Create a new window'
+    }
+])
 
 app.whenReady().then(() => {
     createWindow();
